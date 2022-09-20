31b7ca48dc9084614f2766a8e921fb679375bf87
Meiram Sopy Temirzhanov <random9804@yandex.ru>
2022-09-20 22:17:29 +0600

    added quicknode

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config({ path: "execute.env" });
+require("dotenv").config({ path: ".env" });
 
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
     goerli: {
-      url:`https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY_URL}`,
-      accounts: [GOERLI_PRIVATE_KEY],
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
+
+
+// require("@nomicfoundation/hardhat-toolbox");
+// require("dotenv").config({ path: "execute.env" });
+
+// const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+// const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+
+// module.exports = {
+//   solidity: "0.8.9",
+//   networks: {
+//     goerli: {
+//       url:`https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY_URL}`,
+//       accounts: [GOERLI_PRIVATE_KEY],
+//     },
+//   },
+// };
--- next-app/my-app/constants/index.js
+++ next-app/my-app/constants/index.js
@@ -1,67 +1,67 @@
-export const WHITELIST_CONTRACT_ADDRESS = "0x6a8cC0fe65A6A193190156C1edf35f94E2B8afc9";
+export const WHITELIST_CONTRACT_ADDRESS = "0xaEe340bAda9802E7C7324F583b5a2fbA623DB196";
 export const abi = [
-    {
-      "inputs": [
-        {
-          "internalType": "uint8",
-          "name": "_maxWhitelistedAddresses",
-          "type": "uint8"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [],
-      "name": "addAddressToWhitelist",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "maxWhitelistedAddresses",
-      "outputs": [
-        {
-          "internalType": "uint8",
-          "name": "",
-          "type": "uint8"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "numAddressesWhitelisted",
-      "outputs": [
-        {
-          "internalType": "uint8",
-          "name": "",
-          "type": "uint8"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "name": "whitelistedAddresses",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
+  {
+    "inputs": [
+      {
+        "internalType": "uint8",
+        "name": "_maxWhitelistedAddresses",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "addAddressToWhitelist",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "maxWhitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "numAddressesWhitelisted",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "whitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
 
